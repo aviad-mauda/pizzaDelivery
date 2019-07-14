5931b449a1d05238c8fab4d04835f702a07cd403
Aviad Mauda <aviadm@amdocs.com>
2019-07-14 15:48:13 +0300

    change status to delivered while getting the pizza

--- pizza-delivery/src/main/java/com/example/pizzadelivery/PizzaDeliveryApplication.java
+++ pizza-delivery/src/main/java/com/example/pizzadelivery/PizzaDeliveryApplication.java
@@ -2,8 +2,22 @@ package com.example.pizzadelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+import com.example.pizzadelivery.gateways.consumers.sinks.IDeliveryKafkaConsumer;
+import com.example.pizzadelivery.gateways.producers.sources.IDeliveryKafkaProducer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2	
+@EnableBinding({
+	IDeliveryKafkaProducer.class,
+	IDeliveryKafkaConsumer.class,
+})
+@EnableDiscoveryClient
+
 public class PizzaDeliveryApplication {
 
 	public static void main(String[] args) {
